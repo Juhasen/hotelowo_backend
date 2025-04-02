75c115b0472ce230e474faf0422553cd549057b3
Juhasen <juszczyk-krystian@wp.pl>
2025-04-03 00:00:29 +0200

    Initial

---
+++ src/main/java/pl/juhas/hotelowo_backend/HotelowoBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.juhas.hotelowo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelowoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelowoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/juhas/hotelowo_backend/HotelowoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.juhas.hotelowo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelowoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
