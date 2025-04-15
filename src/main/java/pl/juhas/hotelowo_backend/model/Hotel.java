8fdf31748a65f07c2ab36e8f63356f99246d95eb
Juhasen <juszczyk-krystian@wp.pl>
2025-04-15 23:22:31 +0200

    Added base entities

---
+++ src/main/java/pl/juhas/hotelowo_backend/model/Hotel.java
@@ -0,0 +1,22 @@
+﻿package pl.juhas.hotelowo_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
+
+@Data
+@With
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "hotel")
+public class Hotel {
+    @Id
+    @GeneratedValue
+    private Long id;
+}
