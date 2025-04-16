78ba922714e7d1d890a60e7f28b54be34331a4aa
Juhasen <juszczyk-krystian@wp.pl>
2025-04-16 09:19:46 +0200

    Added fields to Review entity

--- src/main/java/pl/juhas/hotelowo_backend/model/Review.java
+++ src/main/java/pl/juhas/hotelowo_backend/model/Review.java
@@ -1,13 +1,13 @@
 ﻿package pl.juhas.hotelowo_backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.With;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
 
 @Data
 @With
@@ -19,4 +19,29 @@ public class Review{
     @Id
     @GeneratedValue
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "hotel_id", nullable = false)
+    private Hotel hotel;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private String title;
+
+    private String content;
+
+    private int rating;
+
+    private Boolean wouldRecommend;
+
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private LocalDate reviewDate;
+
+    @PrePersist
+    protected void onCreate() {
+        this.reviewDate = LocalDate.now();
+    }
 }
