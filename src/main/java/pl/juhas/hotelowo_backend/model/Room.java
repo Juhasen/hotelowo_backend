0a13ce90c1fef5bd221e3060add20cb53504b953
Juhasen <juszczyk-krystian@wp.pl>
2025-04-16 09:06:46 +0200

    Added fields to Room entity

--- src/main/java/pl/juhas/hotelowo_backend/model/Room.java
+++ src/main/java/pl/juhas/hotelowo_backend/model/Room.java
@@ -1,9 +1,6 @@
 ﻿package pl.juhas.hotelowo_backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,8 +12,24 @@ import lombok.With;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "room")
-public class Room{
+public class Room {
     @Id
     @GeneratedValue
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "hotel_id")
+    private Hotel hotel;
+
+    private String number;
+
+    private String type;
+
+    private String description;
+
+    private int capacity;
+
+    private double price;
+
+    private boolean available;
 }
